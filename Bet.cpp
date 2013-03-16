0351a5b6cb6ba381ae66bdca65f6156f1b45aee3
Robert Adrian Noriega <RNoriega@Utexas.edu>
2013-03-15 19:32:05 -0600

    edit to Bet

--- Bet.cpp
+++ Bet.cpp
@@ -7,3 +7,12 @@ Bet::Bet(int newOwner, int newMoney, int newLocation){
 	location = newLocation;
 	
 }
+int Bet::getOwner(){
+	return owner;
+}
+int Bet::getMoney(){
+	return money;
+}
+int Bet::getLocation(){
+	return location;
+}
